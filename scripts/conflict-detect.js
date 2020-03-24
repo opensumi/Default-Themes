e280c220fa6e6df086ed2091fd881f8a1f557b25
伊北 <taian.lta@antfin.com>
2020-03-24 15:22:03 +0800

    fix: conflict detection

--- scripts/conflict-detect.js
+++ scripts/conflict-detect.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const _ = require('lodash')
 
 const docs = require('./docs')
 
@@ -18,38 +19,39 @@ class ConflictDetect {
         const { data, slug } = cur
         Object.keys(data).forEach(key => {
           const existedValue = prev[key]
+          // console.log(key, existedValue)
           if (existedValue) {
             const themeValue = data[key]
-            if (existedValue !== themeValue) {
-              result.error.push(
-                chalk.error(
-                  `${key}: ${themeValue} is conflicted with existed value in https://lark.alipay.com/ide-framework/ide-token/${slug}`
+            if (_.isEqual(existedValue, themeValue)) {
+              result.warning.push(
+                chalk.yellow(
+                  `${key} is duplicated in https://lark.alipay.com/ide-framework/ide-token/${slug}`
                 )
               )
             } else {
-              result.warning.push(
-                chalk.warning(
-                  `${key} is duplicated in https://lark.alipay.com/ide-framework/ide-token/${slug}`
+              result.error.push(
+                chalk.red(
+                  `${key}: ${themeValue} is conflicted with existed value ${existedValue} in https://lark.alipay.com/ide-framework/ide-token/${slug}`
                 )
               )
             }
           }
         })
-        Object.assign(prev, cur)
+        prev = { ...prev, ...cur.data }
         return prev
       }, {})
 
     if (result.warning.length || result.error.length) {
       console.log('----- Conflict detection results: -----')
       if (result.warning.length) {
-        console.log(chalk.warning('Warnings:'))
+        console.log(chalk.bgYellowBright('Warnings:'))
         result.warning.forEach(msg => {
           console.log(msg)
         })
       }
 
       if (result.error.length) {
-        console.log(chalk.warning('Errors:'))
+        console.log(chalk.bgRedBright('Errors:'))
         result.error.forEach(msg => {
           console.log(msg)
         })
